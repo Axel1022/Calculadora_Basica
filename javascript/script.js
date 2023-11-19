6ad726948b966c6649ef618fee27632329ff194a
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2023-11-19 01:19:41 -0400

    primer commit

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
+}
+function borrar() {
+  document.getElementById("pantalla").value = "";
+}
+function calcular() {
+
+    const valorPantalla = document.getElementById("pantalla").value
+    const result = eval(valorPantalla)
+
+    document.getElementById("pantalla").value = result;
+
+}
